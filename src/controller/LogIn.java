070555e8d0d14e7202a769be76c13276fe122d5c
Vinh Dinh <vinhdinh@outlook.com>
2024-03-07 11:09:49 -0600

    March update

--- src/controller/LogIn.java
+++ src/controller/LogIn.java
@@ -1,4 +1,40 @@
 package controller;
 
-public class LogIn {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LogIn implements Initializable {
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    private Label locationText;
+    @FXML
+    private Label locationLabel;
+
+    // This method is called when the "Log In" button is clicked
+    @FXML
+    private void handleLogin(ActionEvent event) {
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+        // Perform login authentication here
+    }
+
+    // This method is called when the "Exit" button is clicked
+    @FXML
+    private void handleExit(ActionEvent event) {
+        // Close the application
+        System.exit(0);
+    }
+
+    @FXML
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        // Initialization logic
+    }
 }
